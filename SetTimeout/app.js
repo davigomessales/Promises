b9439a067ee76f559cb1235628d8f5f937adf86b
davigomessales <davi.gomes.sales@gmail.com>
2023-02-10 19:29:10 -0300

    add Promises files

---
+++ SetTimeout/app.js
@@ -0,0 +1,24 @@
+/**
+ * 1 - Crie uma função, usingSTO(). Sua função usingSTO() não deve ter parâmetros. Dentro do corpo da função,
+ * deve imprimir uma string no console. Pode ser qualquer string que você quiser.
+ * 2 - Agora, vamos invocar a função setTimeout(). Lembre-se, setTimeout() tem dois parâmetros. Invoque setTimeout()
+ * com sua função usingSTO() como primeiro argumento e um número entre 0 e 3000 como segundo argumento.
+ * 3 - Reserve um momento para prever a saída deste programa.Quando estiver pronto, digite node app.js no terminal e pressione enter.
+
+Veja se a saída do programa no terminal está de acordo com o que você esperava.
+
+ */
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+
+
+
+
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
